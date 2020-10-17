5634643ea462273eee931867896d5a5e42daa95c
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 08:52:13 +0700

    tugas 14 JS
    tugas untuk Niomic

---
+++ tugas14.js
@@ -0,0 +1,15 @@
+let angkaA = 37;
+let tanggalLahir = 16;
+
+let hasil = angkaA / tanggalLahir;
+console.log('Hasil tanpa pembulatan ' + hasil);
+
+// Pembulatan
+// Round
+console.log(Math.round(hasil));
+
+// Ceil
+console.log(Math.ceil(hasil));
+
+// Floor
+console.log(Math.floor(hasil));
